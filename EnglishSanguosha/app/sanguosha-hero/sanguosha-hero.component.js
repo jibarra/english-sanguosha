75a39da2b3905e3560fdb6462010fe232a8856e2
Jose Ibarra <jose.ibarra.dev+git@gmail.com>
2016-12-08 19:31:09 -0700

    Modified sanguosha-hero template to use divs (rows and columns) instead of tables.
    Fixed display on mobile by adding view port to index.html
    
    Removed Angular Smart Table depenency.

--- EnglishSanguosha/app/sanguosha-hero/sanguosha-hero.component.js
+++ EnglishSanguosha/app/sanguosha-hero/sanguosha-hero.component.js
@@ -6,11 +6,26 @@ module('sanguoshaHero', []).
 component('sanguoshaHero', {
     templateUrl: 'sanguosha-hero/sanguosha-hero.template.html',
     controller: function SanguoshaHeroController($http) {
+        var selectedHero = {value: 0};
         var self = this;
-        $http.get('data/sanguosha-heroes.json').then(function(response) {
-            self.displayHeroes = response.data;
-            self.editHeroes = [].concat(self.displayHeroes);
-            // console.log(self.heroes);
-        });
+
+        self.query = function(){
+            $http.get('data/sanguosha-heroes.json').then(function(response) {
+                self.displayHeroes = response.data;
+                // self.editHeroes = [].concat(self.displayHeroes);
+                // console.log(self.heroes);
+            });
+        };
+
+        self.display = function(hero) {
+            if(hero.selected === undefined){
+                hero.selected = true;
+            }
+            else{
+                hero.selected = !hero.selected;
+            }
+        };
+
+        self.query();
     }
 });
